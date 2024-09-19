c9faef0608c03401e795bfe2031e81cc094daecc
Chris <65196986+cjwolfe@users.noreply.github.com>
2024-09-19 16:18:15 -0400

    Update Calculator.java

--- Lab5/Lab05-Template/src/main/apps/Calculator.java
+++ Lab5/Lab05-Template/src/main/apps/Calculator.java
@@ -1,11 +1,20 @@
 package apps;
 
-public Calculator
+import javax.swing.JFrame;
+
+public class Calculator
 {
 	private final int X_LOC = 1;
     private final int Y_LOC = 1;
+    private final int WIDTH = 1;
+    private final int HEIGHT = 1;
+    private final String NAME = "Cool Calculator";
+    private final String RESULT_PREAMBLE = "";
+    private final String ERROR_MESSAGE = "";
+    private JFrame frame;
+    
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!")
+        System.out.println("Hello, World!");
     }
 }
